48f1e07b2d8f62f4b69e14dc2b40c58ecbb7d6fb
sakibmohammad79 <sakibmohammad7679@gmail.com>
2024-05-25 23:02:29 +0600

    password change successfully

--- src/app/(WithDashboardLayout)/dashboard/change-password/page.tsx
+++ src/app/(WithDashboardLayout)/dashboard/change-password/page.tsx
@@ -1,8 +1,96 @@
+"use client";
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+import KeyIcon from "@mui/icons-material/Key";
+import PHForm from "@/Form/PHForm";
+import { removeUser } from "@/services/authService";
+import { authKey } from "@/constant";
+import { toast } from "sonner";
+import PHInput from "@/Form/PHInput";
+import { useChangePasswordMutation } from "@/redux/api/authApi";
+import { FieldValues } from "react-hook-form";
+import { useRouter } from "next/navigation";
 const PasswordChange = () => {
+  const [changePassword] = useChangePasswordMutation();
+  const router = useRouter();
+  const onSubmit = async (values: FieldValues) => {
+    console.log(values);
+    try {
+      const res = await changePassword(values).unwrap();
+      console.log(res);
+      if (res?.success === true) {
+        removeUser();
+        router.push("/");
+        toast.success("Password Changed Successfully");
+      } else {
+        throw new Error("Incorrect Old Password");
+      }
+    } catch (error) {
+      toast.success("Incorrect Old Password");
+      console.log(error);
+    }
+  };
   return (
-    <div>
-      <h2>This is PasswordChange component</h2>
-    </div>
+    <Box
+      sx={{
+        px: 4,
+        py: 2,
+        maxWidth: 600,
+        width: "100%",
+        boxShadow: 1,
+        borderRadius: 1,
+        mx: "auto",
+        mt: {
+          xs: 2,
+          md: 5,
+        },
+      }}
+    >
+      <Stack alignItems="center" justifyContent="center">
+        <Box
+          sx={{
+            "& svg": {
+              width: 100,
+              height: 100,
+            },
+          }}
+        >
+          <KeyIcon sx={{ color: "primary.main" }} />
+        </Box>
+        <Typography variant="h5" fontWeight={600} sx={{ mb: 2, mt: -1.5 }}>
+          Change password
+        </Typography>
+      </Stack>
+      <PHForm
+        onSubmit={onSubmit}
+        //defaultValues={{ oldPassword: "", newPassword: "" }}
+        // resolver={zodResolver(validationSchema)}
+      >
+        <Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <PHInput
+              name="oldPassword"
+              type="password"
+              label="Old Password"
+              fullWidth
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <PHInput
+              name="newPassword"
+              type="password"
+              label="New Password"
+              fullWidth
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+        </Grid>
+
+        <Button type="submit" sx={{ width: "100%", my: 2 }}>
+          change Password
+        </Button>
+      </PHForm>
+    </Box>
   );
 };
 
